e942f61160aa0889f825b510956a48e33d5fe26e
Marcos Souza <bytecore@gmail.com>
2021-05-20 00:27:47 -0300

    Criando o container do postgres

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-import swaggerFile from './swagger.json';
 
+import './database';
+import swaggerFile from './swagger.json';
 import { router } from './routes';
 
 const app = express();
